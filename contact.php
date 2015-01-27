bf65127961370bafe0a8b248659bbcff1c1dd332
liching21 <li_ching92@hotmail.com>
2015-01-27 22:14:23 +1100

    update site title

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
 $emailTo = 'liching.yew@gmail.com';
-$siteTitle = 'YourSiteTitle';
+$siteTitle = 'Liching site'; //'YourSiteTitle'
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
